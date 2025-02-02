2cf22cb69ac6eab35b8ed3d73007625418538af6
rechalub <renatochalub@yahoo.com.br>
2025-02-02 19:32:33 +0000

    Desenvolvendo cartas super trunfo aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,12 +8,13 @@ int main() {
     char estado[55];
     char cidade[30];
     char codigo[10];
-    int populacao;
+    float populacao;
     float area;
     float densidade_populacional;
     float pib;
     float pib_per_capita;
     int pontos_turisticos;
+
     
     
 
@@ -29,35 +30,33 @@ int main() {
     scanf("%s" ,&codigo);
     
     printf("Digite a população:\n");
-    scanf("%d" ,&populacao);
+    scanf("%f" ,&populacao);
 
     printf("Digite a área em km²:\n");
     scanf("%f" ,&area);
 
-    printf("Digite a densidade populacional:\n");
-    scanf("%f" ,&densidade_populacional);
-
     printf("Digite o pib:\n");
     scanf("%f" ,&pib);
-
-    printf("Digite o pib per capita:\n");
-    scanf("%f" ,&pib_per_capita);
-
+    
     printf("Digite quantos pontos turisticos:\n");
     scanf("%d" ,&pontos_turisticos);
 
-    
+//base de caulculos .
+
+    pib_per_capita= (float)(pib / populacao);
+
+    densidade_populacional=(float)(populacao / area);
 
 // O que vai ser imprimido:
 
     printf("nome do estado: %s\n" , estado);
     printf("nome da cidade: %s\n" ,cidade);
     printf("codigo da carta: %s\n" ,codigo);
-    printf("populaçao: %d\n" ,populacao);
-    printf("área em km: %f\n" ,area);
-    printf("densidade populacional: %f\n" ,densidade_populacional);
-    printf("pib: %f\n" ,pib);
-    printf("pib per capita: %f\n" ,pib_per_capita);
+    printf("populaçao: %2f\n" ,populacao);
+    printf("área em km: %.2f\n" ,area);
+    printf("densidade populacional é: %.2f\n" ,densidade_populacional);
+    printf("pib: %.2f\n" ,pib);
+    printf("pib per capita é: %.2f\n" ,pib_per_capita);
     printf("pontos turisticos: %d\n" ,pontos_turisticos);
 
 
